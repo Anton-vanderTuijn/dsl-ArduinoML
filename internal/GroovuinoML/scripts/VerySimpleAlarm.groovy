cd823fd54c50dd633e2a0c4b195ac666c6d4b778
Anton van der Tuijn <anton.van-der-tuijn@etu.unice.fr>
2022-01-19 19:31:06 +0100

    Fix miss understanding with Very Simple Alarm

--- internal/GroovuinoML/scripts/VerySimpleAlarm.groovy
+++ internal/GroovuinoML/scripts/VerySimpleAlarm.groovy
@@ -7,7 +7,7 @@ state "off" means "led" becomes "low" and "buzzer" becomes "low"
 
 initial "off"
 
-from "on" to "off" when "button" becomes "high"
+from "on" to "off" when "button" becomes "low"
 from "off" to "on" when "button" becomes "high"
 
 export "Very Simple Alarm"
